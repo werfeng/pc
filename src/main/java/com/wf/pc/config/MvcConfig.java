c080eddefa3496f777cc05a1f0a17e56aa47d648
xwf <xwf@163.com>
2020-05-25 09:12:33 +0800

    静态资源配置调整

--- src/main/java/com/wf/pc/config/MvcConfig.java
+++ src/main/java/com/wf/pc/config/MvcConfig.java
@@ -1,16 +1,16 @@
-package com.wf.pc.config;
-
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-/**
- * springboot静态资源访问
- */
-public class MvcConfig extends WebMvcConfigurerAdapter {
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        ResourceHandlerRegistration resourceHandlerRegistration = registry.addResourceHandler("/static/**");
-        resourceHandlerRegistration.addResourceLocations("classpath:/static/");
-    }
-}
+//package com.wf.pc.config;
+//
+//import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistration;
+//import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+//
+///**
+// * springboot静态资源访问
+// */
+//public class MvcConfig extends WebMvcConfigurerAdapter {
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        ResourceHandlerRegistration resourceHandlerRegistration = registry.addResourceHandler("/static/**");
+//        resourceHandlerRegistration.addResourceLocations("classpath:/static/");
+//    }
+//}
